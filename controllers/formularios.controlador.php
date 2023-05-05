fd2c14cd2a29cb71ebc0d48abc27d34b340b17b2
Home <jesushome1404@gmail.com>
2023-05-05 11:33:03 -0500

    PDO pt2.2

--- controllers/formularios.controlador.php
+++ controllers/formularios.controlador.php
@@ -18,7 +18,19 @@ class ControladorFormularios{
   // $ Seleccionar Registros
   static public function ctrSeleccionarRegistros(){
     $tabla = "registros";
-    $respuesta = ModeloFormularios::mdlSeleccionarRegistros($tabla);
+    $respuesta = ModeloFormularios::mdlSeleccionarRegistros($tabla, null, null);
     return $respuesta;
+  }
+  // $ Ingreso
+  public function ctrIngreso(){
+    if (isset($_POST["ingresoEmail"])) {
+      $tabla = "registros";
+      $item = "user_email";
+      $valor = $_POST["ingresoEmail"];
+      $respuesta = ModeloFormularios::mdlSeleccionarRegistros($tabla, $item, $valor);
+      if ($respuesta["user_email"] == $_POST["ingresoEmail"] && $respuesta["user_password"] == $_POST["ingresoPassword"]) {
+        echo '<pre>'; print_r($respuesta); echo '</pre>';
+      }
+    }
   }
 }
--- views/pages/ingreso.php
+++ views/pages/ingreso.php
@@ -1,18 +1,46 @@
-<h1>Ingreso</h1>
+<div class="d-flex justify-content-center text-center">
 
-<form action="">
-  <div class="form-group">
-    <label for="email">Email address:</label>
-    <input type="email" class="form-control" placeholder="Enter email" id="email" />
-  </div>
-  <div class="form-group">
-    <label for="pwd">Password:</label>
-    <input type="password" class="form-control" placeholder="Enter password" id="pwd" />
-  </div>
-  <div class="form-group form-check">
-    <label class="form-check-label">
-      <input class="form-check-input" type="checkbox" /> Remember me
-    </label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+  <form class="p-5 bg-light border-radius-" method="post" autocomplete="off" >
+  
+    <div class="form-group">
+      <label for="email">Email:</label>
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <span class="input-group-text">
+            <i class="fa-solid fa-envelope"></i>
+          </span>
+        </div>
+        <input type="email" class="form-control" placeholder="Enter your email..." id="email" name="ingresoEmail"/>
+      </div>
+    </div>
+    
+    <div class="form-group">
+      <label for="pwd">Password:</label>
+      <div class="input-group">
+       <div class="input-group-prepend">
+          <span class="input-group-text">
+           <i class="fa-solid fa-lock"></i>
+          </span>
+        </div>
+        <input type="password" class="form-control" placeholder="Enter your password..." id="pwd" name="ingresoPassword"/>
+      </div>
+    </div>
+
+    <?php
+    
+    // Instaciar Clase Metodo No Estatico (Se ejecuta en el controlador)
+    // $regitro = new ControladorFormularios();
+    // $regitro -> ctrRegistro();
+    
+    // Instaciar Clase Metodo Estatico (reutilizar en la vista para reutilizacion)
+    $registro = new ControladorFormularios;
+    $registro -> ctrIngreso();
+    
+    ?>
+
+    <button type="submit" class="btn btn-primary">Login</button>
+    
+  </form>
+
+</div>
+ 
